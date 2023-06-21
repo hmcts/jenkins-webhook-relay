d6a38407556fe404633a475d2305fadc7b6e488b
mokainos <72137340+mokainos@users.noreply.github.com>
2023-06-21 13:06:10 +0100

    Update variables.tf

--- components/resourcegroup/variables.tf
+++ components/resourcegroup/variables.tf
@@ -1,23 +1,29 @@
 variable "subscription_id" {
   description = "Enter Azure subscription id."
 }
+
 variable "location" {
   description = "Enter Azure location."
   default     = "UK South"
-
 }
+
 variable "env" {
   description = "Enter the environment. eg prod, aat"
+}
 
-}
 variable "product" {
-  description = "The name of the product or project."
+  description = "The name of the product."
 }
+
 variable "builtFrom" {
   description = "The name of the Github repo."
 }
+
 variable "expiresAfter" {
   description = "Expiration date"
   default     = "3000-01-01"
+}
 
+variable "project"{
+  description = "The name of the project."
 }
