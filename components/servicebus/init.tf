f0dc14ea6dbaefc5bd6cd64a83607fea1e8d9732
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-21 16:11:25 +0100

    update

---
+++ components/servicebus/init.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "azurerm" {
+    subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.57.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+provider "azurerm" {
+  features {}
+  alias           = "private_endpoint"
+  subscription_id = var.subscription_id
+}
