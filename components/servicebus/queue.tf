145c4e940542c64045e110352397894a5dc09ecd
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-21 17:07:20 +0100

    update

--- components/servicebus/queue.tf
+++ components/servicebus/queue.tf
@@ -1,6 +1,6 @@
-module "servicebus-queue" {
+/* module "servicebus-queue" {
   source              = "git::https://github.com/hmcts/terraform-module-servicebus-queue?ref=master"
   name                = var.queue_name
   namespace_name      = module.servicebus-namespace.name
   resource_group_name = data.azurerm_resource_group.rg.name
-}
+} */
