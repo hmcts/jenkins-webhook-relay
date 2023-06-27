e6a81c2508944d11ffe8a61f86192a7b4641debe
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-06-27 09:14:49 +0100

    logicapp

--- components/logicapp/data.tf
+++ components/logicapp/data.tf
@@ -6,3 +6,7 @@ data "azurerm_key_vault" "example" {
   name                = var.cft_key_vault_name
   resource_group_name = var.cft_key_vault_rg_name
 }
+data "azurerm_client_config" "current" {}
+data "local_file" "logic_app" {
+  filename = "./workflow.json"
+}
